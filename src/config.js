b38c48af0c1ccdce34b9f795dc861ec4d9ec35f3
hushin <nnnnot+github@gmail.com>
2019-05-05 16:49:29 +0900

    Configure AWS Amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: 'ap-northeast-1',
+    BUCKET: 'notes-app-uploads-t12',
+  },
+  apiGateway: {
+    REGION: 'ap-northeast-1',
+    URL: 'https://cyv7m4y8ul.execute-api.ap-northeast-1.amazonaws.com/prod',
+  },
+  cognito: {
+    REGION: 'ap-northeast-1',
+    USER_POOL_ID: 'ap-northeast-1_rbWRkTRHB',
+    APP_CLIENT_ID: '6md8ilorqsbladeus3r8s0sgsp',
+    IDENTITY_POOL_ID: 'ap-northeast-1:e4d2b177-f75c-44b5-815e-601f6791e18d',
+  },
+};
